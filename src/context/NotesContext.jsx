0f8efcfadbed5e288da8263ffe4638536de26ffe
Coder-Nicki <nicki5389@gmail.com>
2023-05-16 20:23:26 +1000

    added notes provider

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -7,9 +7,58 @@ const initialNotesData = [
         title: "Notes",
         description: "Notes content here",
         isCompleted: false,
-        dueDate: new Date().setDate(new Date().getDate()+1) 
+        dueDate: new Date().setDate(new Date().getDate()+1),
         createdDate: Date.now()
     }
 ]
 
-const notesReducer = 
+const notesReducer = (previousState, instructions) => {
+    let stateEditable = [...previousState];
+
+    switch (instructions.type){
+        case "create":
+            console.log("TODO: Create note and add to state");
+            break;
+        case "update":
+            console.log("TODO: Update specific note and overwrite it");
+            break;
+        case "delete":
+            console.log("TODO: Delete note");
+            break;
+        case "sortbyDueDate":
+            console.log("TODO: sort by due date");
+            break;
+        case "sortbyCreatedAtDate":
+            console.log("TODO: Sort by created date");
+            break;
+        case "sortById":
+            console.log("TODO: Sort by ID");
+            break;
+        default:
+            console.log("Invalid instruction type provided, it was:" + instructions.type)
+            return previousState
+    }
+}
+
+export const NoteDataContext = createContext(null);
+export const NoteDateDispatchContext = createContext(null)
+
+export function useNoteData(){
+    return useContext(NoteDataContext)
+}
+
+export function useNoteDispatch(){
+    return useContext(NoteDataDispatchContext)
+}
+
+export default function NotesProvider(props){
+    const [notesData, notesDispatch] = useReducer(notesReducer, initialNotesData)
+
+    return(
+        <NoteDataContext.Provider value={notesData}>
+            <NoteDateDispatchContext.Provider value={notesDispatch}>
+                {props.children}
+            </NoteDateDispatchContext.Provider>
+        </NoteDataContext.Provider>
+    )
+}
